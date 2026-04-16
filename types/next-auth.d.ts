2f9ba648fd4d8c92d845c724fffbc0bcf2e05a2c
vinbha14 <vinayb2008@gmail.com>
2026-04-16 18:56:06 +0200

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,24 +1,15 @@
 // types/next-auth.d.ts
 import "next-auth"
-import "next-auth/jwt"
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string
       email: string
-      name: string
+      name: string | null
       image?: string | null
       globalRole: string
     }
-  }
-
-  interface User {
-    id: string
-    email: string
-    name: string
-    image?: string | null
-    globalRole: string
   }
 }
 
